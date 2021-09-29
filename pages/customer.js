bbc9d74075b15ef66e72b4a0e879248e4806bb4c
vaishnavi disale <disalevaishnavi@gmail.com>
2021-09-29 17:27:08 +0530

    qr reader updated

--- pages/customer.js
+++ pages/customer.js
@@ -5,6 +5,7 @@ import { Button, Card,Segment } from 'semantic-ui-react';
 import { route } from 'next/dist/server/router';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
+import QrcodeDecoder from 'qrcode-decoder';
 
 
 export default function customer(){
@@ -15,20 +16,41 @@ export default function customer(){
 		})
 	const qrRef = useRef(null);
 
-	// const handleErrorFile = (error) => {
-	//     console.log("err=="+error);
-	//   }
-	//  const handleScanFile = (res) => {
-	//  	 setresult(res);
 
- // 	}
+	const handleFileChange =(event)=> {
+    const {target} = event;
+    const {files} = target;
 
-  // const onScanFile = () => {
-  //   qrRef.current.click();
-  // }
+    if (files && files[0]) {
+      const reader = new FileReader();
+
+      //  reader.onloadstart = () => this.setState({loading: true});
+
+      reader.onload = async event => {
+       // const img = document.getElementById('img');
+      console.log(event.target.result);
+      let result;
+        try {
+          const qr = new QrcodeDecoder();
+         result = await qr.decodeFromImage(event.target.result);
+        } catch (err) {
+          alert("Oops!!! Something went wrong");
+        }
+        if(result)
+	    {	
+	    	window.location.href=result.data;
+	    }
+        console.log("result=="+JSON.stringify(result.data));
+      };
+
+      reader.readAsDataURL(files[0]);
+    }
+  }
+
+
 
 	 const handleErrorWebCam = (error) => {
-    console.log("err=="+error);
+    alert("Oops!!! Something went wrong");
   }
   const handleScanWebCam = (res) => {
   	setresult(res);
@@ -48,8 +70,17 @@ export default function customer(){
 					<h4 style={{color:'black',fontSize:'25px'}}>
 				         Upload your Qr Code
 				    </h4>
-				</div>
-
+					  
+				  	<input
+		              id="file"
+		              type="file"
+		              accept="image/*"
+		              onChange={handleFileChange}
+		            />
+				</div><br/>
+				<div className={styles.title} style={{color:'black',fontSize:'35px'}}>
+					Scan Your QRcode
+				</div><br/>
 				 <QrReader
                          delay={400}
                          style={{width: '30%',height:'30%'}}
