08655a644151a29d835d8b33e173b5fb28d2d2f1
vaishnavi disale <disalevaishnavi@gmail.com>
2021-10-06 16:24:39 +0530

    contract updated and deployed

--- ethereum/admin.js
+++ ethereum/admin.js
@@ -4,7 +4,7 @@ import compiledAdmin from './build/Admin.json';
 
 const instance =  new web3.eth.Contract(
     JSON.parse(compiledAdmin.interface) ,
-    '0x67CA73491cea8f0DCeBCe90bea148FC940C7C829'
+    '0x09a536E6780CBF27aF0b6fdbfADf554864FFb509'
 );
 
 export default instance;
